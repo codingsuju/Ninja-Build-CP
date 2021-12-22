b92237b3c61335c5113f72cc46f9abbc4a957913
codingsuju <codingsuju@gmail.com>
2021-12-22 17:42:42 +0530

    Updated to latest

--- bin/createproblem.bat
+++ bin/createproblem.bat
@@ -3,6 +3,8 @@ mkdir %1
 cd %1
 cp ../../../files/CMakeLists.txt CMakeLists.txt
 cp ../../../files/code.cpp code.cpp
+mkdir bits
+cp ../../../files/stdc++.h bits/stdc++.h
 mkdir build
 cd build
 cmake -G "Ninja" -S .. -B .
